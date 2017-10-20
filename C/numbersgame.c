7c01ebaf050234b700b4fba6ee2f7677e46ffaf3
Ramana <ramanareddy1892@gmail.com>
2017-10-20 16:59:10 +0530

    Remove tabs

--- C/numbersgame.c
+++ C/numbersgame.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-	int i,j,k;
-	printf("Enter a number(1-9)\n");
-	scanf("%d",&i);
-	for(j=0;j<i;j++){
-		for(k=j+1;k<=i;k++)
-			printf("%d ",k);
-		printf("\n");
-	}
-	return 0;
+    int i,j,k;
+    printf("Enter a number(1-9)\n");
+    scanf("%d",&i);
+    for(j=0;j<i;j++){
+        for(k=j+1;k<=i;k++)
+            printf("%d ",k);
+        printf("\n");
+    }
+    return 0;
 }
